9f9138eeb63534b2444f4fb10f531c427b99663e
Bruno Diego Martins <brunodiegom@gmail.com>
2019-07-07 16:10:21 -0300

    Remove logic layer from View at SourceActivity, moving to SourceViewModel

--- app/src/main/java/dev/dextra/newsapp/feature/sources/SourcesActivity.kt
+++ app/src/main/java/dev/dextra/newsapp/feature/sources/SourcesActivity.kt
@@ -1,15 +1,18 @@
 package dev.dextra.newsapp.feature.sources
 
 import android.app.ActivityOptions
+import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import dev.dextra.newsapp.R
 import dev.dextra.newsapp.api.model.Source
 import dev.dextra.newsapp.api.model.enums.Category
 import dev.dextra.newsapp.api.model.enums.Country
-import dev.dextra.newsapp.base.BaseListActivity
 import dev.dextra.newsapp.feature.news.NEWS_ACTIVITY_SOURCE
 import dev.dextra.newsapp.feature.news.NewsActivity
 import dev.dextra.newsapp.feature.sources.adapter.CustomArrayAdapter
@@ -18,29 +21,35 @@ import kotlinx.android.synthetic.main.activity_sources.app_bar
 import kotlinx.android.synthetic.main.activity_sources.category_select
 import kotlinx.android.synthetic.main.activity_sources.country_select
 import kotlinx.android.synthetic.main.activity_sources.sources_list
+import kotlinx.android.synthetic.main.error_state.error_state
+import kotlinx.android.synthetic.main.error_state.error_state_retry
+import kotlinx.android.synthetic.main.error_state.error_state_subtitle
+import kotlinx.android.synthetic.main.error_state.error_state_title
 import org.koin.android.viewmodel.ext.android.viewModel
 
 /**
- * [BaseListActivity] to be launched when the application starts.
+ * [AppCompatActivity] to be launched when the application starts.
  * This class is responsible to present the [Source] content as a list, using the [SourcesViewModel]
  * to have access to the model.
  */
-class SourcesActivity : BaseListActivity(), SourcesListAdapter.SourceListAdapterItemListener {
+class SourcesActivity : AppCompatActivity(), SourcesListAdapter.SourceListAdapterItemListener {
 
     private val sourcesViewModel: SourcesViewModel by viewModel()
 
-    override val emptyStateTitle: Int = R.string.empty_state_title_source
-    override val emptyStateSubTitle: Int = R.string.empty_state_subtitle_source
-    override val errorStateTitle: Int = R.string.error_state_title_source
-    override val errorStateSubTitle: Int = R.string.error_state_subtitle_source
-    override val mainList: View
-        get() = sources_list
-
     private var viewAdapter: SourcesListAdapter = SourcesListAdapter(this)
 
+    private val loadingDialog by lazy {
+        Dialog(this).apply {
+            requestWindowFeature(Window.FEATURE_NO_TITLE)
+            setCancelable(false)
+            window?.setBackgroundDrawableResource(android.R.color.transparent)
+            setContentView(R.layout.dialog_loading)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        setContentView(R.layout.activity_sources)
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sources)
         setupView()
         setupData()
     }
@@ -51,37 +60,70 @@ class SourcesActivity : BaseListActivity(), SourcesListAdapter.SourceListAdapter
         startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
     }
 
-    override fun executeRetry() {
-        setupData()
-    }
-
-    private fun setupData() {
-        observeSource()
-        observeNetworkState()
+    private fun executeRetry() {
         sourcesViewModel.loadSources()
     }
 
-    private fun observeSource() {
-        sourcesViewModel.sources.observe(this, Observer {
-            viewAdapter.apply {
-                clear()
-                notifyDataSetChanged()
-                add(it)
-                notifyDataSetChanged()
-                sources_list.scrollToPosition(0)
-                app_bar.setExpanded(true)
-            }
+    private fun setupLoading() {
+        sourcesViewModel.isLoading.observe(this, Observer<Boolean> { isLoading ->
+            loadingDialog.run { if (isLoading) show() else dismiss() }
         })
     }
 
-    private fun observeNetworkState() {
-        sourcesViewModel.networkState.observe(this, networkStateObserver)
+    private fun setupData() {
+        sourcesViewModel.also {
+            it.sources.observe(this, Observer {
+                viewAdapter.apply {
+                    clear()
+                    notifyDataSetChanged()
+                    add(it)
+                    notifyDataSetChanged()
+                    sources_list.scrollToPosition(0)
+                    app_bar.setExpanded(true)
+                }
+            })
+            it.loadSources()
+        }
     }
 
     private fun setupView() {
         setupCountrySelector()
         setupCategorySelector()
+        setupLoading()
         setupList()
+        setupErrorMessage()
+    }
+
+    private fun setupErrorMessage() {
+        setupErrorVisibility()
+        setupErrorTitle()
+        setupErrorSubtitle()
+        setupRetryButton()
+    }
+
+    private fun setupErrorVisibility() {
+        sourcesViewModel.hasMessage.observe(this, Observer<Boolean> {
+            error_state.visibility = if (it) VISIBLE else GONE
+        })
+    }
+
+    private fun setupErrorTitle() {
+        sourcesViewModel.errorMessageTitle.observe(this, Observer { title ->
+            title?.let { error_state_title.setText(it) }
+        })
+    }
+
+    private fun setupErrorSubtitle() {
+        sourcesViewModel.errorMessageSubtitle.observe(this, Observer { subtitle ->
+            subtitle?.let { error_state_subtitle.setText(it) }
+        })
+    }
+
+    private fun setupRetryButton() {
+        sourcesViewModel.isRetryAllowed.observe(this, Observer<Boolean> {
+            error_state_retry.visibility = if (it) VISIBLE else GONE
+        })
+        error_state_retry.setOnClickListener { executeRetry() }
     }
 
     private fun setupCategorySelector() {
@@ -105,6 +147,9 @@ class SourcesActivity : BaseListActivity(), SourcesListAdapter.SourceListAdapter
     }
 
     private fun setupList() {
+        sourcesViewModel.shouldShowList.observe(this, Observer<Boolean> {
+            sources_list.visibility = if (it) VISIBLE else GONE
+        })
         sources_list.apply {
             setHasFixedSize(true)
             adapter = viewAdapter
--- app/src/main/java/dev/dextra/newsapp/feature/sources/SourcesViewModel.kt
+++ app/src/main/java/dev/dextra/newsapp/feature/sources/SourcesViewModel.kt
@@ -2,6 +2,8 @@ package dev.dextra.newsapp.feature.sources
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import dev.dextra.newsapp.R
 import dev.dextra.newsapp.api.model.Source
 import dev.dextra.newsapp.api.model.enums.Category
 import dev.dextra.newsapp.api.model.enums.Country
@@ -9,7 +11,9 @@ import dev.dextra.newsapp.api.model.extension.getSourceValue
 import dev.dextra.newsapp.api.repository.NewsRepository
 import dev.dextra.newsapp.base.BaseViewModel
 import dev.dextra.newsapp.base.NetworkState
+import dev.dextra.newsapp.base.NetworkState.EMPTY
 import dev.dextra.newsapp.base.NetworkState.ERROR
+import dev.dextra.newsapp.base.NetworkState.RUNNING
 import dev.dextra.newsapp.base.NetworkState.SUCCESS
 import kotlin.properties.Delegates.observable
 
@@ -47,10 +51,41 @@ class SourcesViewModel(private val newsRepository: NewsRepository) : BaseViewMod
     })
 
     /**
+     * Indicates when the server is being accessed.
+     */
+    var isLoading: LiveData<Boolean> = Transformations.map(networkState, this::isLoading)
+
+    /**
+     * Indicates when the [Source] list must be shown.
+     */
+    var shouldShowList: LiveData<Boolean> = Transformations.map(sources, this::shouldShowList)
+
+    /**
+     * Indicates when the retry button must be shown.
+     */
+    var isRetryAllowed: LiveData<Boolean> = Transformations.map(networkState, this::hasError)
+
+    /**
+     * Indicates when the there's a message to be shown.
+     * It should happen when the [NetworkState] is an [ERROR] or [EMPTY].
+     */
+    var hasMessage: LiveData<Boolean> = Transformations.map(networkState, this::hasMessage)
+
+    /**
+     * Text to be set as a title.
+     */
+    var errorMessageTitle: LiveData<Int?> = Transformations.map(networkState, this::getErrorTitle)
+
+    /**
+     * Text to be set as a subtitle.
+     */
+    var errorMessageSubtitle: LiveData<Int?> = Transformations.map(networkState, this::getErrorSubtitle)
+
+    /**
      * Request to server to load the [Source] data, updating the [NetworkState].
      */
     fun loadSources() {
-        setState(NetworkState.RUNNING)
+        setState(RUNNING)
         addDisposable(
             newsRepository.getSources(
                 selectedCountry.getSourceValue(),
@@ -64,7 +99,29 @@ class SourcesViewModel(private val newsRepository: NewsRepository) : BaseViewMod
         )
     }
 
-    private fun getResponseState(sources: List<Source>) = if (sources.isEmpty()) ERROR else SUCCESS
+    private fun isLoading(networkState: NetworkState) = networkState == RUNNING
+
+    private fun hasError(networkState: NetworkState) = networkState == ERROR
+
+    private fun hasEmptyList(networkState: NetworkState) = networkState == EMPTY
+
+    private fun hasMessage(networkState: NetworkState) = hasError(networkState) || hasEmptyList(networkState)
+
+    private fun shouldShowList(sourceList: List<Source>) = sourceList.isNotEmpty()
+
+    private fun getErrorTitle(networkState: NetworkState): Int? = when (networkState) {
+        ERROR -> R.string.error_state_title_source
+        EMPTY -> R.string.empty_state_title_source
+        else -> null
+    }
+
+    private fun getErrorSubtitle(networkState: NetworkState): Int? = when (networkState) {
+        ERROR -> R.string.error_state_subtitle_source
+        EMPTY -> R.string.empty_state_subtitle_source
+        else -> null
+    }
+
+    private fun getResponseState(sources: List<Source>) = if (sources.isEmpty()) EMPTY else SUCCESS
 
     private fun setState(state: NetworkState) {
         _networkState.postValue(state)
